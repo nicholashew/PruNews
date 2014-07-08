a573290a02b411cade5d223226d9af25fd79b35d
Bryant Poole <bryant.poole@asponte.com>
2014-07-08 10:49:57 -0500

    Added trim to remove whitespace

--- PrudentialNews/PrudentialNews/src/com/prudential/vp/HandleDelete.java
+++ PrudentialNews/PrudentialNews/src/com/prudential/vp/HandleDelete.java
@@ -64,9 +64,10 @@ public class HandleDelete implements VirtualPortalScopedAction {
       DocumentId<?> tempDocId = null;
 
       for (int x = 0; x < uuids.length; x++) {
-         tempDocId = thisWorkspace.createDocumentId(uuids[x]);
+    	  String thisUUID = uuids[x].trim();
+         tempDocId = thisWorkspace.createDocumentId(thisUUID);
          if (isDebug) {
-            s_log.log(Level.FINEST, "Value: " + uuids[x]);
+            s_log.log(Level.FINEST, "Value: " + thisUUID);
             s_log.log(Level.FINEST, "Retrieved docId = " + tempDocId);
          }
          docIdList.add(tempDocId);
